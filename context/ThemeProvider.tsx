f6af954ab1e19a649a67c6c49d73edff07bf1152
Rajeev <rajeevkumarjbo2003@gmail.com>
2023-12-14 17:24:58 +0530

    leftsidebar added

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -12,13 +12,16 @@ const ThemeContext=createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({children}:{children:React.ReactNode}){
     const [mode, setMode] = useState('');
     const handleThemeChange=()=>{
-       if(mode==='dark'){
+       if(localStorage.theme ==='dark' ||
+       (!("theme" in localStorage) && 
+       window.matchMedia("(prefers-color-scheme: dark)").matches)
+       ){
         setMode('dark');
         document.documentElement.classList.add('dark');
        }
        else{
         setMode('light');
-        document.documentElement.classList.add('light');
+        document.documentElement.classList.remove('dark');
        }
     }
     
