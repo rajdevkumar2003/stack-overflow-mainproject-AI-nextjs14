fa32d44a6756250c306a5da22d232a89ef664211
Rajeev <rajeevkumarjbo2003@gmail.com>
2023-12-27 11:31:41 +0530

    correction done

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,13 @@
 import Question from '@/components/forms/Question'
 import { getUserById } from '@/lib/actions/user.action';
+import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Page = async () => {
-  // const { userId } = auth();
+ const { userId } = auth();
 
-  const userId = 'CL123'
+
   if (!userId) redirect('/sign-in');
 
   const mongoUser = await getUserById({ userId });
